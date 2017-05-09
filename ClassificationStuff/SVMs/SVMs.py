7a4f29862cc114167bc84aa4a10a4ead83943b78
Alex Wolf <alexander.r.wolf.18@dartmouth.edu>
2017-05-09 18:10:58 -0400

    made KNN optimal neighbir search func

--- ClassificationStuff/SVMs/SVMs.py
+++ ClassificationStuff/SVMs/SVMs.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from sklearn.linear_model import SGDClassifier
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Imputer, StandardScaler
+
 from ClassificationStuff.classificationData import getWineData
 
 
@@ -64,44 +68,6 @@ def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), fol
     #penalty, loss, and class weights
     steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
              ('scaler', preprocess),
-             ('kernelSVM', SGDClassifier(kernel=kernel, penalty= penalty, loss=loss, class_weight=class_weight ))]
-
-    pipeline = Pipeline(steps)
-
-    # create different alphas to test
-    stepsize = (aHigh - aLow) / numAlphas
-    alphas = np.arange(aLow, aHigh, stepsize)
-
-    # create different l1Ratios to test
-    stepsize = 1 / numL1Ratios
-    l1_ratios = np.arange(l1RatLow, l1RatHigh, stepsize)
-
-    param_grid = {'linearSVM__alpha': alphas,
-                  'linearSVM__l1_ratio': l1_ratios}
-
-    # Create the GridSearchCV
-    gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
-
-    # fit the Grid Search Cross Value Model
-    gm_cv.fit(X_train, y_train)
-
-    # Compute and print metrics
-    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
-    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
-    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
-
-
-def performNoNLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
-                     class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
-
-
-    # use hold out validation for analysis
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
-
-    # create pipeline for Model testing/training
-    #penalty, loss, and class weights
-    steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
-             ('scaler', preprocess),
              ('linearSVM', SGDClassifier(penalty= penalty, loss=loss, class_weight=class_weight ))]
 
     pipeline = Pipeline(steps)
@@ -129,10 +95,44 @@ def performNoNLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), 
     print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
 
 
+def performKNN(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, nLow=1, nHigh=5, nIter=1):
 
 
-def performNoNLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
-                     class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
+    # use hold out validation for analysis
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+    # create pipeline for Model testing/training
+    steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
+             ('scaler', preprocess),
+             ('knn', KNeighborsClassifier())]
+
+    pipeline = Pipeline(steps)
+
+    # compute KNN array
+    kSizes = np.arange(nLow, nHigh, nIter)
+
+    param_grid = {'knn__n_neighbors': kSizes}
+
+    # Create the GridSearchCV
+    gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
+
+    # fit the Grid Search Cross Value Model
+    gm_cv.fit(X_train, y_train)
+
+    # Compute and print metrics
+    print("Knn Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best Neighbor: " + str(gm_cv.best_params_.get('knn__n_neighbors')))
+
+
+"""
+Kernel types
+    -rbf
+    -poly
+
+"""
+
+def performNoNLinearSVM(X, y, kernel='rbf', impStrategy= 'mean', preprocess=StandardScaler(), folds=5, loss='hinge', cLow=.000001, cHigh=1, numCs=10,
+                        gammaLow=0, gammaHigh=1, numGammas=10, class_weight=None):
 
 
     # use hold out validation for analysis
@@ -142,35 +142,36 @@ def performNoNLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), 
     #penalty, loss, and class weights
     steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
              ('scaler', preprocess),
-             ('linearSVM', SGDClassifier(penalty= penalty, loss=loss, class_weight=class_weight ))]
+             ('nonLinearSVM', SVC(kernel=kernel, probability=True, class_weight=class_weight))]
 
     pipeline = Pipeline(steps)
 
-    # create different alphas to test
-    stepsize = (aHigh - aLow) / numAlphas
-    alphas = np.arange(aLow, aHigh, stepsize)
+    # create different c (penalty) values to test
+    stepsize = (cHigh - cLow) / numCs
+    Cs = np.arange(cLow, cHigh, stepsize)
 
     # create different l1Ratios to test
-    stepsize = 1 / numL1Ratios
-    l1_ratios = np.arange(l1RatLow, l1RatHigh, stepsize)
+    stepsize = 1 / numGammas
+    Gammas = np.arange(gammaLow, gammaHigh, stepsize)
 
-    param_grid = {'linearSVM__alpha': alphas,
-                  'linearSVM__l1_ratio': l1_ratios}
+    param_grid = {'nonLinearSVM__C': Cs,
+                  'nonLinearSVM__gamma': Gammas}
 
     # Create the GridSearchCV
     gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
+
 
     # fit the Grid Search Cross Value Model
     gm_cv.fit(X_train, y_train)
 
     # Compute and print metrics
-    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
-    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
-    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
+    print("NonLinear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best C (Penalty Of Error term): " + str(gm_cv.best_params_.get('nonLinearSVM__C')))
+    print("Best Gamma Value : " + str(gm_cv.best_params_.get('nonLinearSVM__gamma')))
 
 
 
 
 X, y= getWineData()
 
-performLinearSVM(X, y, folds=5, aHigh= .1, numL1Ratios=100)
+performKNN(X, y)
