399f6c5647b7025a0ded076ff1e03bd928eb32cd
Alex Wolf <alexander.r.wolf.18@dartmouth.edu>
2017-05-09 16:50:52 -0400

    adding nonlinear SVM

--- ClassificationStuff/SVMs/SVMs.py
+++ ClassificationStuff/SVMs/SVMs.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import SGDClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
@@ -52,7 +53,45 @@ Linear SVM  using SGDClassifier()
         
 
 """
-def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=1, numAlphas=10,
+def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
+                     class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
+
+
+    # use hold out validation for analysis
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+    # create pipeline for Model testing/training
+    #penalty, loss, and class weights
+    steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
+             ('scaler', preprocess),
+             ('kernelSVM', SGDClassifier(kernel=kernel, penalty= penalty, loss=loss, class_weight=class_weight ))]
+
+    pipeline = Pipeline(steps)
+
+    # create different alphas to test
+    stepsize = (aHigh - aLow) / numAlphas
+    alphas = np.arange(aLow, aHigh, stepsize)
+
+    # create different l1Ratios to test
+    stepsize = 1 / numL1Ratios
+    l1_ratios = np.arange(l1RatLow, l1RatHigh, stepsize)
+
+    param_grid = {'linearSVM__alpha': alphas,
+                  'linearSVM__l1_ratio': l1_ratios}
+
+    # Create the GridSearchCV
+    gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
+
+    # fit the Grid Search Cross Value Model
+    gm_cv.fit(X_train, y_train)
+
+    # Compute and print metrics
+    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
+    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
+
+
+def performNoNLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
                      class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
 
 
@@ -84,15 +123,54 @@ def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), fol
     # fit the Grid Search Cross Value Model
     gm_cv.fit(X_train, y_train)
 
-    # Predict the labels of the test set: y_pred
-    y_pred = gm_cv.predict(X_test)
+    # Compute and print metrics
+    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
+    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
+
+
+
+
+def performNoNLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
+                     class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
+
+
+    # use hold out validation for analysis
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+    # create pipeline for Model testing/training
+    #penalty, loss, and class weights
+    steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
+             ('scaler', preprocess),
+             ('linearSVM', SGDClassifier(penalty= penalty, loss=loss, class_weight=class_weight ))]
+
+    pipeline = Pipeline(steps)
+
+    # create different alphas to test
+    stepsize = (aHigh - aLow) / numAlphas
+    alphas = np.arange(aLow, aHigh, stepsize)
+
+    # create different l1Ratios to test
+    stepsize = 1 / numL1Ratios
+    l1_ratios = np.arange(l1RatLow, l1RatHigh, stepsize)
+
+    param_grid = {'linearSVM__alpha': alphas,
+                  'linearSVM__l1_ratio': l1_ratios}
+
+    # Create the GridSearchCV
+    gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
+
+    # fit the Grid Search Cross Value Model
+    gm_cv.fit(X_train, y_train)
 
     # Compute and print metrics
-    print("Best Alpha: " + str(gm_cv.best_params_))
-    print(classification_report(y_test, y_pred))
-    print("Tuned Model Parameters: {}".format(gm_cv.best_params_))
+    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
+    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
+
+
 
 
 X, y= getWineData()
 
-performLinearSVM(X, y, folds=10, aHigh=1, numL1Ratios=10)
+performLinearSVM(X, y, folds=5, aHigh= .1, numL1Ratios=100)
