0a4ef511b2262d8af495c46cc889841b68129d42
Alex Wolf <alexander.r.wolf.18@dartmouth.edu>
2017-05-09 23:08:14 -0400

    finished classification functions with documentation

--- ClassificationStuff/classificationFunctions.py
+++ ClassificationStuff/classificationFunctions.py
@@ -38,8 +38,8 @@ Methods:
     performMultiNomNB(X, y, binaryData=False, folds=5, impStrategy= 'mean', preprocess=MaxAbsScaler(),
                       aLow=0, aHigh=1, numAlphas=3, fit_prior=False, class_prior=None)
         -performs a MultiNominal NB
-        -if Data Binary set binaryData to False
-        -Alpha is smoothness parmater tested in crossfold search
+        -if Binary Data set binaryData to False
+        -Alpha is smoothness paramater tested in crossfold search
             - 0 means no smoothness applied
     
     performKNN(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, nLow=1, nHigh=5, nIter=1):
@@ -52,6 +52,7 @@ Methods:
     
     performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge,
      aLow = 0.0001, aHigh=.1, numAlphas=10, class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10)
+     -test different hyparamters L1Ration, and Alphas through Hold out validation, with grid search
         -penlatly 'l2', 'l1', or 'elasticnet'
             -l2 and net lead to sparse data
             -Use when LOOK INTO MORE
@@ -73,83 +74,48 @@ Methods:
             -regualarization term
          
      
-     performNoNLinearSVM(X, y, kernel='rbf', impStrategy= 'mean', preprocess=StandardScaler(), folds=5, cLow=.000001, cHigh=1,
-                        numCs=10,gammaLow=0, gammaHigh=1, numGammas=10, class_weight=None)
-        
-        - 
-    
-    
-    
-    
-                      
-    
-    
+     performNoNLinearSVM(X, y, kernel='rbf', impStrategy= 'mean', preprocess=StandardScaler(), folds=5, cLow= 1, cHigh=10,
+                        numCs=10, gammaLow=0, gammaHigh=1, numGammas=10, class_weight=None)
+        -test different hyparamters c, and gamma through hold out validation, with grid search
+            -kernel
+                -'rbf', 'sigmoid', 'ploy'
+            -C
+                -soft margin cost
+                -Larger C == bigger soft Margin
+                - Use K cross validation to find
+            -gamma
+                -larger gamma leads to hiah bias low variance          
+            -class_weight
+                -set if class imbalance
+                
 """
 
-
-"""
-Linear SVM  using SGDClassifier()
-    - params
-    
-    impStrategy
-        -mean deafult
-    preprocess
-        -deafults to StandardScaler()
-        
-    penalty
-        -'l2' deafult
-        -'l1'
-        - ‘elasticnet’
-    loss
-        - 'hinge'
-        - 'log'- logisitc regression
-        - ‘modified_huber'- good if outliers
-        - ‘squared_hinge’- quadratcillay penalized 
-    
-    
-    class_weight='None'
-        -'balanced'
-        custom dictionary of entried
-        
-        
-    hyperParamTesting
-        -alpha stuff
-        -l1 ratio
-    
-    chooseFrom Data Analysis
-        -penalty
-        -loss
-    
-        
-    
-        
-
-"""
-def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
-                     class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
-
+def performMultiNomNB(X, y, binaryData=False, folds=5, impStrategy= 'mean', preprocess=MaxAbsScaler(),
+                      aLow=0, aHigh=1, numAlphas=3, fit_prior=False, class_prior=None):
 
     # use hold out validation for analysis
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    #use bernulli or Multinominal Classifier based if binary data or not
+    if binaryData:
+        Nbclf= BernoulliNB(fit_prior=fit_prior, class_prior=class_prior)
+    else:
+        Nbclf= MultinomialNB(fit_prior=fit_prior, class_prior=class_prior)
 
     # create pipeline for Model testing/training
     #penalty, loss, and class weights
     steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
              ('scaler', preprocess),
-             ('linearSVM', SGDClassifier(penalty= penalty, loss=loss, class_weight=class_weight, random_state=2))]
+             ('nb', Nbclf)]
 
     pipeline = Pipeline(steps)
+    print(pipeline.get_params().keys())
 
     # create different alphas to test
     stepsize = (aHigh - aLow) / numAlphas
     alphas = np.arange(aLow, aHigh, stepsize)
 
-    # create different l1Ratios to test
-    stepsize = 1 / numL1Ratios
-    l1_ratios = np.arange(l1RatLow, l1RatHigh, stepsize)
-
-    param_grid = {'linearSVM__alpha': alphas,
-                  'linearSVM__l1_ratio': l1_ratios}
+    param_grid = {'nb__alpha': alphas}
 
     # Create the GridSearchCV
     gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
@@ -158,55 +124,26 @@ def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), fol
     gm_cv.fit(X_train, y_train)
 
     # Compute and print metrics
-    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
-    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
-    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
+    print("NB Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best Alpha (smoothing paramter): " + str(gm_cv.best_params_.get('nb__alpha')))
 
 
-"""
-Kernel types
-    -rbf
-    -poly
+def performGuassianNB(X, y, folds=10, impStrategy= 'mean', preprocess=MaxAbsScaler(), priors=None):
 
-"""
-
-def performNoNLinearSVM(X, y, kernel='rbf', impStrategy= 'mean', preprocess=StandardScaler(), folds=5, cLow=.000001, cHigh=1,
-                        numCs=10,gammaLow=0, gammaHigh=1, numGammas=10, class_weight=None):
-
-
-    # use hold out validation for analysis
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
 
     # create pipeline for Model testing/training
-    #penalty, loss, and class weights
     steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
              ('scaler', preprocess),
-             ('nonLinearSVM', SVC(kernel=kernel, probability=True, class_weight=class_weight, random_state=2))]
+             ('knn', GaussianNB(priors=priors))]
 
     pipeline = Pipeline(steps)
 
-    # create different c (penalty) values to test
-    stepsize = (cHigh - cLow) / numCs
-    Cs = np.arange(cLow, cHigh, stepsize)
-
-    # create different l1Ratios to test
-    stepsize = 1 / numGammas
-    Gammas = np.arange(gammaLow, gammaHigh, stepsize)
-
-    param_grid = {'nonLinearSVM__C': Cs,
-                  'nonLinearSVM__gamma': Gammas}
-
-    # Create the GridSearchCV
-    gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
-
-
-    # fit the Grid Search Cross Value Model
-    gm_cv.fit(X_train, y_train)
+    # Get accuracy scores via Cross Validation
+    bestScore = np.mean(cross_val_score(pipeline, X, y, cv=folds))
 
     # Compute and print metrics
-    print("NonLinear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
-    print("Best C (Penalty Of Error term): " + str(gm_cv.best_params_.get('nonLinearSVM__C')))
-    print("Best Gamma Value : " + str(gm_cv.best_params_.get('nonLinearSVM__gamma')))
+    print("Guassian NB Accuracy: {}\n".format(bestScore))
+
 
 
 def performKNN(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, nLow=1, nHigh=5, nIter=1):
@@ -269,48 +206,31 @@ def performRandomForest(X, y, folds=5, impStrategy= 'mean', class_weight=None, p
     print("Best tree Amount: " + str(gm_cv.best_params_.get('forest__n_estimators')))
 
 
-def performGuassianNB(X, y, folds=10, impStrategy= 'mean', preprocess=MaxAbsScaler(), priors=None):
+def performLinearSVM(X, y, impStrategy= 'mean', preprocess=StandardScaler(), folds=5, penalty='l2', loss='hinge', aLow = 0.0001, aHigh=.1, numAlphas=10,
+                     class_weight=None, l1RatLow=0, l1RatHigh=.5, numL1Ratios=10):
 
-
-    # create pipeline for Model testing/training
-    steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
-             ('scaler', preprocess),
-             ('knn', GaussianNB(priors=priors))]
-
-    pipeline = Pipeline(steps)
-
-    # Get accuracy scores via Cross Validation
-    bestScore = np.mean(cross_val_score(pipeline, X, y, cv=folds))
-
-    # Compute and print metrics
-    print("Guassian NB Accuracy: {}\n".format(bestScore))
-
-
-def performMultiNomNB(X, y, binaryData=False, folds=5, impStrategy= 'mean', preprocess=MaxAbsScaler(),
-                      aLow=0, aHigh=1, numAlphas=3, fit_prior=False, class_prior=None):
 
     # use hold out validation for analysis
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-    #use bernulli or Multinominal Classifier based if binary data or not
-    if binaryData:
-        Nbclf= BernoulliNB(fit_prior=fit_prior, class_prior=class_prior)
-    else:
-        Nbclf= MultinomialNB(fit_prior=fit_prior, class_prior=class_prior)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
 
     # create pipeline for Model testing/training
+    #penalty, loss, and class weights
     steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
              ('scaler', preprocess),
-             ('nb', Nbclf)]
+             ('linearSVM', SGDClassifier(penalty= penalty, loss=loss, class_weight=class_weight, random_state=2))]
 
     pipeline = Pipeline(steps)
-    print(pipeline.get_params().keys())
 
     # create different alphas to test
     stepsize = (aHigh - aLow) / numAlphas
     alphas = np.arange(aLow, aHigh, stepsize)
 
-    param_grid = {'nb__alpha': alphas}
+    # create different l1Ratios to test
+    stepsize = 1 / numL1Ratios
+    l1_ratios = np.arange(l1RatLow, l1RatHigh, stepsize)
+
+    param_grid = {'linearSVM__alpha': alphas,
+                  'linearSVM__l1_ratio': l1_ratios}
 
     # Create the GridSearchCV
     gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
@@ -319,14 +239,45 @@ def performMultiNomNB(X, y, binaryData=False, folds=5, impStrategy= 'mean', prep
     gm_cv.fit(X_train, y_train)
 
     # Compute and print metrics
-    print("NB Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
-    print("Best Alpha (smoothing paramter): " + str(gm_cv.best_params_.get('nb__alpha')))
+    print("Linear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best Alpha (Learning Rate): " + str(gm_cv.best_params_.get('linearSVM__alpha')))
+    print("Best L1 Ratio : " + str(gm_cv.best_params_.get('linearSVM__l1_ratio')))
 
 
+def performNoNLinearSVM(X, y, kernel='rbf', impStrategy= 'mean', preprocess=StandardScaler(), folds=5, cLow=1, cHigh=10,
+                        numCs=10,gammaLow=0, gammaHigh=1, numGammas=10, class_weight=None):
 
 
+    # use hold out validation for analysis
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+    # create pipeline for Model testing/training
+    #penalty, loss, and class weights
+    steps = [('imputation', Imputer(missing_values='NaN', strategy=impStrategy, axis=0)),
+             ('scaler', preprocess),
+             ('nonLinearSVM', SVC(kernel=kernel, probability=True, class_weight=class_weight, random_state=2))]
+
+    pipeline = Pipeline(steps)
+
+    # create different c (penalty) values to test
+    stepsize = (cHigh - cLow) / numCs
+    Cs = np.arange(cLow, cHigh, stepsize)
+
+    # create different l1Ratios to test
+    stepsize = 1 / numGammas
+    Gammas = np.arange(gammaLow, gammaHigh, stepsize)
+
+    param_grid = {'nonLinearSVM__C': Cs,
+                  'nonLinearSVM__gamma': Gammas}
+
+    # Create the GridSearchCV
+    gm_cv = GridSearchCV(pipeline, param_grid, cv=folds)
 
 
-X, y= getWineData()
+    # fit the Grid Search Cross Value Model
+    gm_cv.fit(X_train, y_train)
 
-performNoNLinearSVM(X, y)
+    # Compute and print metrics
+    print("NonLinear SVM Accuracy: {}\n".format(gm_cv.score(X_test, y_test)))
+    print("Best C (Penalty Of Error term): " + str(gm_cv.best_params_.get('nonLinearSVM__C')))
+    print("Best Gamma Value : " + str(gm_cv.best_params_.get('nonLinearSVM__gamma')))
