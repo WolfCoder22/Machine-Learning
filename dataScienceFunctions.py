556f36ea0d519dcefad6d1b451f21968e5103b98
Alex Wolf <alexander.r.wolf.18@dartmouth.edu>
2017-05-10 15:25:09 -0400

    outlier checking function

--- dataScienceFunctions.py
+++ dataScienceFunctions.py
@@ -10,9 +10,25 @@ figsize=[8, 8]
 
 """
 Functions to Analyze Data
-    - change figure size with global above
-    - 
+    - change figure size with Global Var above
+    
+    
+Plotting:
+        
+    scatterMatrixPlot(isCategorical, dfX, dfY=None, diagonal='kde')
+        -plot scatter matrix
+        - if categorical maek iscategorical as true and enter DfY for scatter coloring
+        -diagnoal
+            -'kde'- density plot of vars
+            -'hist'- histogram of vars
+    
+Expolatory Data Anlaysis:
 
+    is_outlier(points, stdThresh=3.5):
+        -checks outliers in a data set with threshold
+        -return Boolean array of indexes
+        -code from StackOverflow
+        
 """
 
 """
@@ -30,8 +46,43 @@ def scatterMatrixPlot(isCategorical, dfX, dfY=None, diagonal='kde'):
     #maximize window and show
     plt.show()
 
+def is_outlier(points, stdThresh=3.5):
+    """
+    Returns a boolean array with True if points are outliers and False 
+    otherwise.
+
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
+
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+    """
+    if len(points.shape) == 1:
+        points = points[:,None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > stdThresh
+
 dfX, dfY= getWineData(True)
-scatterMatrixPlot(False, dfX.iloc[:, [1, 2, 3, 4 ,5, 6]], dfY)
+#scatterMatrixPlot(False, dfX.iloc[:, [1, 2, 3, 4 ,5, 6]], dfY)
+
+print(is_outlier(dfX))
 
 
 
